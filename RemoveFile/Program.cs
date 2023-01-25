2a623ab2eefd281beba17bda5c63fac43bc2617e
DerAstrit <astrit.selimi20@hotmail.com>
2023-01-25 23:00:37 +0100

    Finish

--- RemoveFile/Program.cs
+++ RemoveFile/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// remove a selected file from the directory
+
+Console.WriteLine("Remove a file from the disk \n\n" +
+                  "Enter the directory path");
+string? path = Console.ReadLine();
+
+Console.WriteLine("\nEnter the file name to be removed");
+string? filename = Console.ReadLine();
+    
+string filepath = path + "\\" + filename;
+
+Console.WriteLine("-------------------------------------------------------");
+
+if (File.Exists(filepath))
+{
+    File.Delete(filepath);
+    Console.WriteLine($"The File {filename} deleted successfully");
+}
+else
+{
+    Console.WriteLine("File does not exist");
+}
